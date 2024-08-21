4f805c8ac34dcc674d2a5f133882fb848aed2880
彭博文 <8016203+peng-bowen@user.noreply.gitee.com>
2024-08-21 19:10:23 +0800

    学习完3个效果

---
+++ 3D_Hover_Effect/index.js
@@ -0,0 +1,28 @@
+const card = document.querySelector(".card");
+const yRange = [-10, 10];
+
+/**
+ * @param {number} length 鼠标可移动的全长
+ * @param {number} value 鼠标所在位置
+ * @param {Array} range range取值范围
+ * 通过 鼠标所在位置 比上 鼠标可移动的全长 === 返回值 比上 range取值范围
+ */
+function getRotateDeg(length, value, range) {
+  return (value / length) * (range[1] - range[0]) + range[0];
+}
+
+card.addEventListener("mousemove", (e) => {
+  const { offsetX, offsetY } = e;
+  const { offsetWidth, offsetHeight } = card;
+  // x 轴旋转靠的是 鼠标的y
+  let rx = getRotateDeg(offsetWidth, offsetY, yRange);
+  let ry = -getRotateDeg(offsetHeight, offsetX, yRange);
+  card.style.setProperty("--rx", `${rx}deg`);
+  card.style.setProperty("--ry", `${ry}deg`);
+});
+
+// 移出置为0
+card.onmouseleave = (e) => {
+  card.style.setProperty("--rx", "0deg");
+  card.style.setProperty("--ry", "0deg");
+};
